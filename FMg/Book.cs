6b38144358cebe7d3d828ee6c3098acb3d00c19a
ravimhingele <aivanova261203@gmail.com>
2023-05-25 01:25:52 +0300

    Ready

--- FMg/Book.cs
+++ FMg/Book.cs
@@ -11,12 +11,21 @@ namespace FMg
         public string Title { get; set; }
         public string Link { get; set; }
 
+        public string Author { get; set; }
+        public string Rating { get; set; }
+        public string Price { get; set; }
+        public string Date { get; set; }
+
         //public string Author { get; set; }
 
-        public Book(string title, string link)
+        public Book(string title, string link, string author, string rating, string price, string date)
         {
             Title = title;
             Link = link;
+            Author = author;
+            Rating = rating;
+            Price = price;
+            Date = date;
         }
 
         public override string ToString()
--- FMg/Form1.cs
+++ FMg/Form1.cs
@@ -17,19 +17,24 @@ using System.Windows.Forms;
 using HtmlAgilityPack;
 using System.Net;
 
+
 namespace FMg
 {
     public partial class Form1 : Form
     {
         Label label1 = new Label();
-        
+
         ListBox listBox = new ListBox();
+
+        ListView listView = new ListView();
        
         Button ReturnButton = new Button();
 
         Button FindButton = new Button();
 
         ComboBox comboBox = new ComboBox();
+
+        NumericUpDown numOfBooks = new NumericUpDown();
 
         WebClient Client;
 
@@ -67,7 +72,7 @@ namespace FMg
             //---TextBox---//
             comboBox.Location = new Point(3, 2);
             comboBox.Height = 30;
-            comboBox.Width = 570;
+            comboBox.Width = 490;
             comboBox.Font = GlobalFont;
             Controls.Add(comboBox);
             string[] exampleSearches = { "Python", "C++", "Java" };
@@ -76,17 +81,71 @@ namespace FMg
                 comboBox.Items.Add(item);
             }
 
+
+            numOfBooks.Location = new Point(505, 2);
+            numOfBooks.Font = new Font(CurrentUser.FontFamily, 12);
+            numOfBooks.Maximum = 100;
+            numOfBooks.Minimum = 10;
+            numOfBooks.Width = 70;
+            Controls.Add(numOfBooks);
+
+            
+            //--ListView--//
+            ColumnHeader column1 = new ColumnHeader();
+            ColumnHeader column2 = new ColumnHeader();
+            ColumnHeader column3 = new ColumnHeader();
+            ColumnHeader column4 = new ColumnHeader();
+            ColumnHeader column5 = new ColumnHeader();
+
+            column1.Text = "Title";
+            column2.Text = "Author";
+            column3.Text = "Rating";
+            column4.Text = "Price";
+            column5.Text = "Date";
+
+
+            column1.Width = 260;
+            column2.Width = 200;
+            column3.Width = 110;
+            column4.Width = 100;
+            column5.Width = 100;
+
+
+            // добавляем столбцы в ListView
+            listView.View = View.Details;
+            listView.Columns.Add(column1);
+            listView.Columns.Add(column2);
+            listView.Columns.Add(column3);
+            listView.Columns.Add(column4);
+            listView.Columns.Add(column5);
+            listView.Location = new Point(1, 35);
+            listView.Height = 390;
+            listView.Width = 780;
+            listView.Font = GlobalFont;
+            listView.ItemActivate += new EventHandler(listView_ItemActivate);
+            Controls.Add(listView);
+            
+
+
+
+
+
+            
             //--ListBox--//
             listBox.Location = new Point(3, 35);
             listBox.Height = 390; //433
             listBox.Width = 677;
             listBox.Font = GlobalFont;
             listBox.BackColor = CurrentUser.BackgroundColor;
-            Controls.Add(listBox);
+            //Controls.Add(listBox);
+            
+
+
+
 
 
             //---Размер окна---//
-            SetSize(700, 500);
+            SetSize(800, 500);
             this.Text = "Файловый менеджер"; 
             CenterToScreen();
 
@@ -126,55 +185,6 @@ namespace FMg
             listBox.BackColor = backColor;
         }
 
-        private void AddDirectoriesAndFiles(TreeNode node, DirectoryInfo parent)
-        {
-            try
-            {
-                foreach (DirectoryInfo directory in parent.GetDirectories())
-                {
-                    TreeNode childNode = new TreeNode(directory.Name);
-                    childNode.Tag = directory.FullName;
-                    node.Nodes.Add(childNode);
-                    AddDirectoriesAndFiles(childNode, directory);
-                }
-
-                foreach (FileInfo file in parent.GetFiles())
-                {
-                    TreeNode childNode = new TreeNode(file.Name);
-                    childNode.Tag = file.FullName;
-                    node.Nodes.Add(childNode);
-                }
-            }
-            catch (Exception ex)
-            {
-                // Обработка ошибок доступа к файлам или каталогам.
-            }
-        }
-
-        
-
-        private void GetDirectories(DirectoryInfo[] subDirs, TreeNode nodeToAddTo)
-        {
-            TreeNode aNode;
-            DirectoryInfo[] subSubDirs;
-            foreach (DirectoryInfo subDirInfo in subDirs)
-            {
-                try
-                {
-                    aNode = new TreeNode(subDirInfo.Name, 0, 0);
-                    aNode.Tag = subDirInfo;
-                    aNode.ImageKey = "folder";
-                    if (subDirInfo.GetDirectories().Length > 0)
-                    {
-                        subSubDirs = subDirInfo.GetDirectories();
-                        GetDirectories(subSubDirs, aNode);
-                    }
-                    nodeToAddTo.Nodes.Add(aNode);
-                }
-                catch { }
-            }
-        }
-
 
 
         private void ListBox_MouseDoubleClick(object sender, MouseEventArgs e)
@@ -182,10 +192,17 @@ namespace FMg
             Book currentBook = listBox.SelectedItem as Book;
             Process.Start(currentBook.Link);
         }
-        
 
 
+        private void listView_ItemActivate(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                ListViewItem item = listView.SelectedItems[0];
+                Process.Start((string)item.Tag);
+            }
 
+        }
         //---Функции кнопок---
 
         private void ReturnButton_Click(object sender, EventArgs e)
@@ -194,7 +211,6 @@ namespace FMg
             {
                 path = Path.GetDirectoryName(path);
             }
-            //ShowFiles(path);
         }
 
         private void SettingsButton_Click(object sender, EventArgs e)
@@ -208,46 +224,84 @@ namespace FMg
         private void FindButton_Click(object sender, EventArgs e)
         {
             Client = new WebClient();
-            string searchUrl = "https://www.amazon.com/s?k=python&i=stripbooks-intl-ship&ref=nb_sb_noss";
-            string htmlCode = Client.DownloadString(searchUrl);
-            HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
-            document.LoadHtml(htmlCode);
-            string xpath = "//div[@data-component-type='s-search-result']";
-            HtmlNodeCollection bookNodes = document.DocumentNode.SelectNodes(xpath);
-            if (bookNodes != null)
-            {
-                List<Book> books = new List<Book>();
 
-                foreach (HtmlNode node in bookNodes)
+            Client.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0");
+            Client.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
+            Client.Headers.Add("Accept-Encoding", "br");
+            Client.Headers.Add("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3");
+
+
+            List<Book> books = new List<Book>();
+            int PageNumber = 1;
+            while (books.Count < numOfBooks.Value)
+            {
+                string re = comboBox.Text;
+                string searchUrl = $"https://www.amazon.com/s?k={CreateRef(re)}&i=stripbooks-intl-ship&page={PageNumber}&ref=nb_sb_noss";
+                string htmlCode = Client.DownloadString(searchUrl);
+                HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
+                document.LoadHtml(htmlCode);
+                string xpath = "//div[@data-component-type='s-search-result']";
+                HtmlNodeCollection bookNodes = document.DocumentNode.SelectNodes(xpath);
+                if (bookNodes != null)
                 {
-                    var titleNode = node.SelectSingleNode(".//span[@class='a-size-medium a-color-base a-text-normal']");
-                    string title = titleNode?.InnerText.Trim();
 
-                    // Получаем ссылку на книгу
-                    var linkNode = node.SelectSingleNode(".//a[contains(@class, 'a-link-normal') and contains(@class, 's-underline-text') and contains(@class, 's-underline-link-text') and contains(@class, 's-link-style') and contains(@class, 'a-text-normal')]");
-                    string link = linkNode?.GetAttributeValue("href", "");
 
-                    if (!string.IsNullOrEmpty(title) && !string.IsNullOrEmpty(link))
+                    foreach (HtmlNode node in bookNodes)
                     {
-                        books.Add(new Book(title, "https://www.amazon.com" + link));
+                        if (books.Count < numOfBooks.Value)
+                        {
+                            var titleNode = node.SelectSingleNode(".//span[@class='a-size-medium a-color-base a-text-normal']");
+                            string title = titleNode?.InnerText.Trim();
+
+                            var authorNode = node.SelectSingleNode(".//a[contains(@class, 'a-size-base a-link-normal s-underline-text s-underline-link-text s-link-style')]");
+                            string author = authorNode?.InnerText.Trim();
+
+                            var ratingNode = node.SelectSingleNode(".//span[contains(@class, 'a-icon-alt')]");
+                            string rating = ratingNode?.InnerText.Trim();
+
+                            var priceNode = node.SelectSingleNode(".//span[contains(@class, 'a-offscreen')]");
+                            string price = priceNode?.InnerText.Trim();
+
+
+                            var dateNode = node.SelectSingleNode(".//span[contains(@class, 'a-size-base a-color-secondary a-text-normal')]");
+                            string date = dateNode?.InnerText.Trim();
+
+                            // Получаем ссылку на книгу
+                            var linkNode = node.SelectSingleNode(".//a[contains(@class, 'a-link-normal') and contains(@class, 's-underline-text') and contains(@class, 's-underline-link-text') and contains(@class, 's-link-style') and contains(@class, 'a-text-normal')]");
+                            string link = linkNode?.GetAttributeValue("href", "");
+
+                            if (!string.IsNullOrEmpty(title) && !string.IsNullOrEmpty(link))
+                            {
+                                books.Add(new Book(title, "https://www.amazon.com" + link, author, rating, price, date));
+                            }
+                        }
                     }
+                    ShowResult(books);
                 }
-                ShowResult(books);
-            }
-            else
-            {
-                MessageBox.Show("Ничего не найдено. Попробуйте переформулировать свой запрос");
+                else
+                {
+                    MessageBox.Show("Ничего не найдено. Попробуйте переформулировать свой запрос");
+                }
+                PageNumber++;
             }
         }
 
 
         public void ShowResult(List<Book> books)
         {
+            listView.Items.Clear();
+            foreach (Book book in books)
+            {
+                ListViewItem item = new ListViewItem(new[] {book.Title, book.Author, book.Rating, book.Price, book.Date});
+                item.Tag = book.Link;
+                listView.Items.Add(item);
+            }
+            /*
             listBox.Items.Clear();
             foreach (Book book in books)
             {
                 listBox.Items.Add(book);
-            }
+            }*/
         }
 
 
@@ -273,6 +327,12 @@ namespace FMg
         {
             button.Height = 35;
             button.Width = 35;
+        }
+
+        private string CreateRef(string r)
+        {
+            r.Replace(" ", "+");
+            return r;
         }
 
 
